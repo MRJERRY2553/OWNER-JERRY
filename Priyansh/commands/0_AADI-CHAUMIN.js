38457f3be81829ca33d4aff898c7e88836a0ab2b
MRJERRY2553 <144557435+MRJERRY2553@users.noreply.github.com>
2025-02-09 15:33:15 +0500

    Update 0_JERRY-CHAUMIN.js

--- Priyansh/commands/0_AADI-CHAUMIN.js
+++ Priyansh/commands/0_AADI-CHAUMIN.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 module.exports.config = {
-        name: "chaumin",
+        name: "chumi",
     version: "1.0.1",
         hasPermssion: 0,
         credits: "AADI BABU", 
@@ -12,9 +12,9 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
-        if (event.body.indexOf("chaumin")==0 || event.body.indexOf("Chaumin")==0 || event.body.indexOf("CHAUMIN")==0 || event.body.indexOf("chauminn")==0) {
+        if (event.body.indexOf("chumi")==0 || event.body.indexOf("Chummi")==0 || event.body.indexOf("CHUMMI")==0 || event.body.indexOf("chummi")==0) {
                 var msg = {
-                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 \n< ────────────────── >\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐜𝐇𝐚𝐔𝐦𝐈𝐧 \n< ────────────────── >",
+                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐌𝐫 𝐉𝐞𝐫𝐫𝐲 \n< ────────────────── >\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐂𝐡𝐔𝐦𝐈 \n< ────────────────── >",
                                 attachment: fs.createReadStream(__dirname + `/noprefix/8a9f84ed741b6df2854fb136dd028e79.jpg`)
                         }
                         api.sendMessage(msg, threadID, messageID);
